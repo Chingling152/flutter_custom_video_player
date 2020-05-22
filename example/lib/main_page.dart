8383ec6c238ae32831b9c06c550e3c8c12b3cbdc
Franklin <franklin.bdantas@hotmail.com>
2020-05-21 22:28:56 -0300

    Example project added

---
+++ example/lib/main_page.dart
@@ -0,0 +1,17 @@
+import 'package:custom_video_player/custom_video_player.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
+
+class MainPage extends StatefulWidget{
+  @override
+  State<MainPage> createState()=> _MainPageState();
+}
+
+class _MainPageState extends State<MainPage> {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      body: CustomVideoPlayer(),
+    );
+  }
+}
